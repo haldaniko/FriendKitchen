10076a0ca50883a8bbd11f83684fd0ce2b9b92e4
haldaniko <haldaniko@gmail.com>
2026-03-02 14:44:40 +0100

    fix

--- FriendKitchen/src/components/Header/Header.tsx
+++ FriendKitchen/src/components/Header/Header.tsx
@@ -2,15 +2,23 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import kitchen from '../../assets/kitchen.svg'
 
+const getHomeHref = () => {
+    if (window.location.pathname === '/kitchen' || window.location.pathname.startsWith('/kitchen/')) {
+        return '/kitchen/';
+    }
+
+    return '/';
+};
+
 const Header = () => {
     return (
         <header className={styles.header}>
-            <Link to="/" className={styles.logoContainer}>
+            <a href={getHomeHref()} className={styles.logoContainer}>
                 <div className={styles.logoWrapper}>
                     <img src={kitchen} alt="Logo" />
                 </div>
                 <h1 className={styles.title}>Кухня на Дружбата</h1>
-            </Link>
+            </a>
 
             <nav className={styles.nav}>
                 <Link to="/editing" className={styles.editBtn}>
